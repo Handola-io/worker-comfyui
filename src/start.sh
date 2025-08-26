a4a55840de3e4bcf740c713161cc96bb0926584c
Handola <iHandola@gmail.com>
2025-08-26 21:39:50 +0200

    fix: extra launch arguments for ComfyUI

--- src/start.sh
+++ src/start.sh
@@ -11,16 +11,18 @@ echo "worker-comfyui: Starting ComfyUI"
 
 # Allow operators to tweak verbosity; default is DEBUG.
 : "${COMFY_LOG_LEVEL:=DEBUG}"
+# Launch args for comfy ui.
+: "${COMFY_EXTRA_LAUNCH_ARGS:=--highvram --gpu-only}"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --listen --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
-
+    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --listen \
+        --verbose "${COMFY_LOG_LEVEL}" --log-stdout ${COMFY_EXTRA_LAUNCH_ARGS} &
     echo "worker-comfyui: Starting RunPod Handler"
     python -u /handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
-    python -u /comfyui/main.py --disable-auto-launch --disable-metadata --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
-
+    python -u /comfyui/main.py --disable-auto-launch --disable-metadata \
+        --verbose "${COMFY_LOG_LEVEL}" --log-stdout ${COMFY_EXTRA_LAUNCH_ARGS} &
     echo "worker-comfyui: Starting RunPod Handler"
     python -u /handler.py
 fi
